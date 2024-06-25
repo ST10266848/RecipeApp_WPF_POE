d6620f00e71bffc57603df55b707461c70b9827f
ST10266848 <ST10266848@vcconnect.edu.za>
2024-06-25 18:23:16 +0200

    Second Commit
    - Added multiple windows in my WPF Recipe App, including the designing and implementing a menu window called 'MainWindow' with name of the app and several options represented as buttons.
    - In future, when a user selects one of these options by clicking on their respective button, the menu window will close and a window dedicated to the particular option will be displayed, allowing the user to enter or view details if needed.

--- RecipeApp_WPF/MainWindow.xaml.cs
+++ RecipeApp_WPF/MainWindow.xaml.cs
@@ -20,5 +20,53 @@ namespace RecipeApp_WPF
         {
             InitializeComponent();
         }
+
+        private void AddRecipeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddRecipeWindow addRecipe = new AddRecipeWindow();
+            this.Hide();
+            addRecipe.ShowDialog();
+            this.Show();
+        }
+
+        private void ViewRecipesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ViewRecipesWindow viewRecipes = new ViewRecipesWindow();
+            this.Hide();
+            viewRecipes.ShowDialog();
+            this.Show();
+        }
+
+        private void ScaleRecipeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ScaleRecipeWindow scaleRecipe = new ScaleRecipeWindow();
+            this.Hide();
+            scaleRecipe.ShowDialog();
+            this.Show();
+        }
+
+        private void RevertRecipeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RevertRecipeWindow revertRecipe = new RevertRecipeWindow();
+            this.Hide();
+            revertRecipe.ShowDialog();
+            this.Show();
+        }
+
+        private void ClearRecipeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClearRecipeWindow clearRecipe = new ClearRecipeWindow();
+            this.Hide();
+            clearRecipe.ShowDialog();
+            this.Show();
+        }
+
+        private void ExitAppBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ExitAppWindow exitApp = new ExitAppWindow();
+            this.Hide();
+            exitApp.ShowDialog();
+            this.Show();
+        }
     }
 }
